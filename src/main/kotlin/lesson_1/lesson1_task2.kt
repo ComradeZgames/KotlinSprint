db5d674f4a8919bd980842ee7a96931e7e6a828c
ASUS <psych0svkk@gmail.com>
2024-08-14 16:25:40 +0500

    KS-1-1 complete task

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_1
+
+fun main() {
+    val countOfOrders: Int = 75
+    val textReview: String = "Спасибо!"
+
+    println("Количество заказов: $countOfOrders\nОтзыв о заказе: $textReview")
+
+//    val countOfWorkers: Int = 2000
+//    println("Количество работников: $countOfWorkers")
+
+    val newCountOfWorkers: Int = 1999
+    println("Новое количество работников: $newCountOfWorkers")
+}
