af6d70dde55137ed72ff655bfe1cb27012029278
ASUS <psych0svkk@gmail.com>
2024-08-16 11:30:09 +0500

    KS-2-1 complete task

---
+++ src/main/kotlin/lesson_2/lesson2_task2.kt
@@ -0,0 +1,11 @@
+package org.example.lesson_2
+
+fun main() {
+    val countOfStudents = 4
+    val evaluation1 = 3.0
+    val evaluation2 = 4.0
+    val evaluation3 = 3.0
+    val evaluation4 = 5.0
+
+    println((evaluation1 + evaluation2 + evaluation3 + evaluation4) / countOfStudents)
+}
