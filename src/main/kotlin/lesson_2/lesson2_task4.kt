1373c3843898e3dae4ccba1b9cb3f7fb82cafd47
ComradeZ <81288167+ComradeZgames@users.noreply.github.com>
2024-08-19 12:34:14 +0500

    Update lesson2_task4.kt
    minor fix#2

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -5,7 +5,7 @@ fun main() {
     val ironOreCount = 11
     val crystalOreCount = 7
 
-    val bonusIronOre = (ironOreCount * (BUFF_PERCENT/100f)).toInt()
+    val bonusIronOre = (ironOreCount * (buffPercent/100f)).toInt()
     val bonusCrystalOre = (crystalOreCount * BUFF_PERCENT).toInt()
 
     println("Bonus amount of iron ore: $bonusIronOre\nBonus amount of crystal ore: $bonusCrystalOre")
