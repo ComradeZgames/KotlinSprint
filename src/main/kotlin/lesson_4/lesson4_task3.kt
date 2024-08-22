aec75cdabdf8ea897531ef3ca79ea9163dcf5ea4
ASUS <psych0svkk@gmail.com>
2024-08-22 13:03:58 +0500

    KS-4-3 fix

--- src/main/kotlin/lesson_4/lesson4_task3.kt
+++ src/main/kotlin/lesson_4/lesson4_task3.kt
@@ -1,5 +1,10 @@
 package org.example.lesson_4
 
+const val SUNNY_WEATHER = true
+const val OUTDOOR_AWNING = true
+const val AIR_MOISTURE = 20
+const val UNFAVORABLE_SEASON = "winter"
+
 fun main() {
     val sunnyWeather = true
     val outdoorAwning = true
@@ -8,6 +13,6 @@ fun main() {
 
     println(
         "Благоприятные ли условия сейчас для роста бобовых? " +
-                "${sunnyWeather && outdoorAwning && (airMoisture == 20) && (season != "winter")}"
+                "${sunnyWeather == SUNNY_WEATHER && outdoorAwning == OUTDOOR_AWNING && (airMoisture == AIR_MOISTURE) && (season != UNFAVORABLE_SEASON)}"
     )
 }
