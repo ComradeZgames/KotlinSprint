7f83ba705f16b3b8e49db6af65da07c57f1c2bb1
ASUS <psych0svkk@gmail.com>
2024-08-22 13:09:09 +0500

    KS-4-4 fix

--- src/main/kotlin/lesson_4/lesson4_task4.kt
+++ src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -1,16 +1,13 @@
 package org.example.lesson_4
 
 fun main() {
-    var trainingDay = 1
-    val armExercises = (trainingDay % 2 != 0)
-    val legExercises = (trainingDay % 2 == 0)
-    val backExercises = (trainingDay % 2 == 0)
-    val abdominalExercises = (trainingDay % 2 != 0)
+    val trainingDay = 1
+    val isOddDay = (trainingDay % 2 != 0)
 
     println("""
-        Упражнения для рук:    $armExercises
-        Упражнения для ног:    $legExercises
-        Упражнения для спины:  $backExercises
-        Упражнения для пресса: $abdominalExercises
+        Упражнения для рук:    $isOddDay
+        Упражнения для ног:    ${!isOddDay}
+        Упражнения для спины:  ${!isOddDay}
+        Упражнения для пресса: $isOddDay
     """.trimIndent())
 }
