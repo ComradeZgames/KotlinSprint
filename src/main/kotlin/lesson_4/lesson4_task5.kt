abb4b06b324920e91b83a615332883d0b367a69b
ASUS <psych0svkk@gmail.com>
2024-08-23 07:25:52 +0500

    KS-4-5 add const, remove comments

--- src/main/kotlin/lesson_4/lesson4_task5.kt
+++ src/main/kotlin/lesson_4/lesson4_task5.kt
@@ -1,5 +1,11 @@
 package org.example.lesson_4
 
+const val DAMAGED = false
+const val CREW_COUNT_MAX = 70
+const val CREW_COUNT_MIN = 55
+const val BOX_COUNT_MIN = 50
+const val FAVORABLE_WEATHER = true
+
 fun main() {
     print("Укажите наличие повреждений корпуса (true/false): ")
     val damaged: Boolean = readln().toBoolean()
@@ -13,12 +19,9 @@ fun main() {
     print("Укажите благоприятны ли погодные условия? (true/false)")
     val favorableWeather: Boolean = readln().toBoolean()
 
-    val allowedToSail = (!damaged && (crewCount in 55..70) && (boxCount > 50)) ||
-            ((crewCount == 70) && (boxCount >= 50) && favorableWeather)
-
-    /*хотел сделать так, но узнал что тернарного оператора в котлине нет,
-    "println(allowedToSail? "Корабль может отправиться в плавание" : "Корабль не может отправиться в плавание")"
-    поэтому для красивого вывода, я сделал следующее*/
+    val allowedToSail =
+        (damaged == DAMAGED && (crewCount in CREW_COUNT_MIN..CREW_COUNT_MAX) && (boxCount > BOX_COUNT_MIN)) ||
+            ((crewCount == CREW_COUNT_MAX) && (boxCount >= BOX_COUNT_MIN) && favorableWeather == FAVORABLE_WEATHER)
 
     println(
         if (allowedToSail) "Корабль может отправиться в плавание"
